ff8ad3fc5038edb6742731a47cd54f076c45d5a6
LeCaoDat <lecaodat1909@gmail.com>
2018-06-11 14:59:12 +0700

    List tasks not done, done

---
+++ src/components/TaskNotDone.vue
@@ -0,0 +1,30 @@
+<template>
+  <li class="row ui-state-default">
+    <div class="col-md-9">
+      {{ task.content }}
+    </div>
+    <div class="col-md-3">
+      <button class="btn btn-default">
+        <i class="fa fa-edit"></i>
+      </button>
+      <button class="btn btn-default">
+        <i class="fa fa-check"></i>
+      </button>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TaskNotDone',
+  data () {
+    return {
+    }
+  },
+  props: ['task']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
